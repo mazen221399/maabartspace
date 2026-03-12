b685615c2f3103a3cd778992152362423a047fc2
Mazin Andijani <mmandijani1@gmail.com>
2026-03-12 15:24:24 +0300

    add instagram link

--- app/page.tsx
+++ app/page.tsx
@@ -106,7 +106,7 @@ export default function HomePage() {
             </a>
 
             <a
-              href="https://instagram.com"
+              href="https://instagram.com/maab.artspace"
               target="_blank"
               rel="noreferrer"
               style={{
