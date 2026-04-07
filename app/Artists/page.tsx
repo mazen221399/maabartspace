f9c550b4fcf4cfe09e3ae80ae12d7839992d2718
Mazin Andijani <mmandijani1@gmail.com>
2026-04-07 11:10:20 +0300

    add second artist

--- app/Artists/page.tsx
+++ app/Artists/page.tsx
@@ -3,12 +3,12 @@ export default function ArtistsPage() {
     <main style={{ padding: "60px 20px", textAlign: "center" }}>
       <h1 style={{ marginBottom: "40px" }}>الفنانون</h1>
 
-      {/* مربع الصورة */}
+      {/* الفنان الأول */}
       <div
         style={{
           width: "320px",
           height: "420px",
-          margin: "0 auto",
+          margin: "0 auto 40px",
           backgroundColor: "#f2d23b",
           display: "flex",
           alignItems: "center",
@@ -18,22 +18,52 @@ export default function ArtistsPage() {
       >
         <img
           src="/images/artist1.jpg"
-          alt="artist"
-          style={{
-            width: "85%",
-            height: "auto",
-          }}
+          alt="artist1"
+          style={{ width: "85%", height: "auto" }}
         />
       </div>
 
-      {/* النص + الرابط */}
-      <p style={{ marginTop: "25px", lineHeight: "1.8" }}>
-        للتعرف أكثر على الفنان، يرجى زيارة حسابنا على{" "}
+      {/* رابط انستجرام */}
+      <p style={{ marginBottom: "60px" }}>
+        للتعرف أكثر على الفنان، يرجى زيارة{" "}
         <a
           href="https://instagram.com/maab.artspace"
           target="_blank"
           rel="noopener noreferrer"
-          style={{ color: "#000", fontWeight: "bold" }}
+          style={{ fontWeight: "bold" }}
+        >
+          إنستجرام
+        </a>
+      </p>
+
+      {/* الفنان الثاني */}
+      <div
+        style={{
+          width: "320px",
+          height: "420px",
+          margin: "0 auto 40px",
+          backgroundColor: "#f2d23b",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          overflow: "hidden",
+        }}
+      >
+        <img
+          src="/images/artist2.jpg"
+          alt="artist2"
+          style={{ width: "85%", height: "auto" }}
+        />
+      </div>
+
+      {/* رابط انستجرام */}
+      <p>
+        للتعرف أكثر على الفنان، يرجى زيارة{" "}
+        <a
+          href="https://instagram.com/maab.artspace"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ fontWeight: "bold" }}
         >
           إنستجرام
         </a>
